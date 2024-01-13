edfc3ecec86e5eba10397b11ef9ba68d784e2b4b
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-01-13 15:07:15 +0100

    Adds useWindowSize hook
    returns the height and width of the window.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,9 +1,11 @@
 import useData from "./useData";
 import useRenderChartProps from "./useRenderChartProps";
 import useFontContext from "./useFontContext";
+import useWindowSize from "./useWindowSize";
 
 export {
     useData,
     useRenderChartProps,
     useFontContext,
+    useWindowSize,
 }
