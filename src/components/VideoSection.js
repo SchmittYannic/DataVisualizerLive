1fc284ac6f0ca95364122bd284885a5a08e23c49
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-01-13 15:13:35 +0100

    Makes VideoSection responsive for mobile

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,14 +1,21 @@
 import { useEffect, useRef, useState } from "react";
+
 import { uploadwebm, datawebm, viswebm } from "../assets";
-import VideoProgress2 from "./VideoProgress2";
+import VideoProgress from "./VideoProgress";
+import { useWindowSize } from "../hooks";
 import "./VideoSection.css";
 
 const VideoSection = () => {
     const uploadwebmRef = useRef(null);
     const datawebmRef = useRef(null);
     const viswebmRef = useRef(null);
+    const windowSize = useWindowSize();
 
     const [currentVideo, setCurrentVideo] = useState(null);
+
+    const isProgressBarVertical = windowSize.width < 680 ? false : windowSize.width < 1140 ? false : true;
+    const videoWidth = windowSize.width < 680 ? 344.2 : windowSize.width < 1140 ? 480 : 640;
+    const videoHeight = windowSize.width < 680 ? 258.15 : windowSize.width < 1140 ? 360 : 480;
 
     const handleVideoEnded = (ref) => {
         const nextVideo = ref.current;
@@ -24,19 +31,19 @@ const VideoSection = () => {
         <section className="video-section">
             <div className="video-section-left">
                 <div className={`video-section-left-item ${currentVideo === uploadwebmRef.current ? "active" : ""}`}>
-                    <VideoProgress2 videoRef={uploadwebmRef} currentVideo={currentVideo} />
+                    <VideoProgress videoRef={uploadwebmRef} currentVideo={currentVideo} vertical={isProgressBarVertical} />
                     <h4>Upload</h4>
                     <p>Der erste Schritt besteht aus dem Hochladen einer Datei. DataVisualizer unterstützt das Datenformat: csv.</p>
                 </div>
 
                 <div className={`video-section-left-item ${currentVideo === datawebmRef.current ? "active" : ""}`}>
-                    <VideoProgress2 videoRef={datawebmRef} currentVideo={currentVideo} />
+                    <VideoProgress videoRef={datawebmRef} currentVideo={currentVideo} vertical={isProgressBarVertical} />
                     <h4>Datenansicht</h4>
                     <p>Hier wird ein Einblick in den Datensatz gewährt. An dieser Stelle wird sichergestellt, dass der Datensatz erfolgreich hochgeladen wurde.</p>
                 </div>
 
                 <div className={`video-section-left-item ${currentVideo === viswebmRef.current ? "active" : ""}`}>
-                    <VideoProgress2 videoRef={viswebmRef} currentVideo={currentVideo} />
+                    <VideoProgress videoRef={viswebmRef} currentVideo={currentVideo} vertical={isProgressBarVertical} />
                     <h4>Visualisierung</h4>
                     <p>Durch eine Vielzahl an Einstellungen kann hier ein Diagramm erstellt und angepasst werden.</p>
                     <br />
@@ -47,8 +54,8 @@ const VideoSection = () => {
             <div className="video-section-right">
                 <video 
                     ref={uploadwebmRef}
-                    width="640"
-                    height="480"
+                    width={videoWidth}
+                    height={videoHeight}
                     onCanPlay={() => setCurrentVideo(uploadwebmRef.current)}
                     onEnded={() => handleVideoEnded(datawebmRef)}
                     style={{
@@ -61,8 +68,8 @@ const VideoSection = () => {
 
                 <video 
                     ref={datawebmRef}
-                    width="640"
-                    height="480"
+                    width={videoWidth}
+                    height={videoHeight}
                     onEnded={() => handleVideoEnded(viswebmRef)}
                     style={{
                         zIndex: currentVideo === datawebmRef.current ? 2 : -1
@@ -74,8 +81,8 @@ const VideoSection = () => {
 
                 <video 
                     ref={viswebmRef}
-                    width="640"
-                    height="480"
+                    width={videoWidth}
+                    height={videoHeight}
                     onEnded={() => handleVideoEnded(uploadwebmRef)}
                     style={{
                         zIndex: currentVideo === viswebmRef.current ? 2 : -1
