57232cb5a51a4b2ef92ee698a3d7e49ad75236ea
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-01-23 01:39:50 +0100

    Adds new DataProvider and renames old provider

--- src/context/DataProvider.js
+++ src/context/DataProvider.js
@@ -1,13 +1,16 @@
+import { toJSON, DataFrame } from "danfojs";
 import { useState, createContext, useEffect } from "react";
-import { AutoData, catColumnsAuto, numColumnsAuto, dateColumnsAuto } from "../data/AutoData";
-import { WetterData, catColumnsWetter, numColumnsWetter, dateColumnsWetter } from "../data/WetterData";
+import { AutoData } from "../data/AutoData";
+import { WetterData } from "../data/WetterData";
 import { placeholderString } from "../constants";
 
 const initContextState = {
-    sessionId: null,
-    setSessionId: () => {},
     fileIsUploaded: false,
     setFileIsUploaded: () => {},
+    demodata: "",
+    setDemodata: () => {},
+    dataframe: null,
+    setDataframe: () => {},
     dataAsJSON: [],
     setDataAsJSON: () => {},
     dataAsJSONLength: 0,
@@ -30,14 +33,18 @@ const initContextState = {
     setDateOptions: () => {},
     dataCopy: [],
     setDataCopy: () => {},
-}
+    operations: {},
+    setOperations: () => {},
+    isLoading: false,
+    setIsLoading: () => {},
+};
 
 export const DataContext = createContext(initContextState);
 
 export const DataProvider = ({children}) => {
     const [fileIsUploaded, setFileIsUploaded] = useState(false);
     const [demodata, setDemodata] = useState("");
-
+    const [dataframe, setDataframe] = useState(null);
     const [dataAsJSON, setDataAsJSON] = useState([]);
     const [dataAsJSONLength, setDataAsJSONLength] = useState(0);
 
@@ -52,44 +59,94 @@ export const DataProvider = ({children}) => {
     const [dateOptions, setDateOptions] = useState([]);
     const [dataCopy, setDataCopy] = useState([]);
 
+    const [operations, setOperations] = useState({
+        setDataframe: false,
+        setJSONLength: false,
+        setCatOptions: false,
+        setDateOptions: false,
+    });
+    const [isLoading, setIsLoading] = useState(false);
+
     useEffect(() => {
-        if(demodata === "useAutoData"){
-            setDataAsJSON(AutoData);
-            setCatColumns(catColumnsAuto);
-            setNumColumns(numColumnsAuto);
-            setDateColumns(dateColumnsAuto);
+        if (demodata === "useAutoData") {
+            const df = new DataFrame(AutoData);
+            setDataframe(df);
         }
         if(demodata === "useWetterData"){
-            setDataAsJSON(WetterData);
-            setCatColumns(catColumnsWetter);
-            setNumColumns(numColumnsWetter);
-            setDateColumns(dateColumnsWetter);
+            const df = new DataFrame(WetterData);
+            setDataframe(df);
         }
     }, [demodata]);
 
-    //calculate and set Arraylengths whenever the corresponding Array changes
     useEffect(() => {
+        if (!dataframe) {
+            setFileIsUploaded(false);
+            setIsLoading(false);
+            return
+        }
+        
+        setFileIsUploaded(true);
+        setDataAsJSON(toJSON(dataframe));
+
+        const columnNames = dataframe.columns;
+        const uniqueColumns = dataframe.nUnique(0).$dataIncolumnFormat;
+        const dtypes = dataframe.dtypes;
+
+        const catColumnsArray = [];
+        const numColumnsArray = [];
+        const dateColumnsArray = [];
+
+        for (let column in columnNames) {
+            if (uniqueColumns[column] <= 10) {
+                catColumnsArray.push(columnNames[column]);
+            }
+            if (dtypes[column] === "int32" || dtypes[column] === "float32") {
+                numColumnsArray.push(columnNames[column]);
+            }
+            if (dtypes[column] === "string") {
+                dateColumnsArray.push(columnNames[column]);
+            }
+        }
+
+        setCatColumns(catColumnsArray);
+        setCatColumnsLength(catColumnsArray.length);
+        setNumColumns(numColumnsArray);
+        setNumColumnsLength(numColumnsArray.length);
+        setDateColumns(dateColumnsArray);
+
+        setOperations((prevState) => {
+            const newState = {...prevState};
+            newState.setDataframe = true;
+            return newState
+        });
+    }, [dataframe]);
+
+    useEffect(() => {
+        if (!fileIsUploaded) return
+
         setDataAsJSONLength(dataAsJSON.length);
-    }, [dataAsJSON]);
 
-    useEffect(() => {
-        setCatColumnsLength(catColumns.length);
-    }, [catColumns]);
-
-    useEffect(() => {
-        setNumColumnsLength(numColumns.length);
-    }, [numColumns]);
-
-    useEffect(() => {
-        setDateColumnsLength(dateColumns.length);
-    }, [dateColumns]);
+        setOperations((prevState) => {
+            const newState = {...prevState};
+            newState.setJSONLength = true;
+            return newState
+        });
+    }, [dataAsJSON, fileIsUploaded]);
 
     //create a copy of catColumns called catOptions, which includes a placeholderString.
     useEffect(() => {
+        if (!fileIsUploaded) return
+
         const catOptions = JSON.parse(JSON.stringify(catColumns));
         if (!catOptions.includes(placeholderString)) catOptions.unshift(placeholderString);
         setCatOptions(catOptions);
-    }, [catColumns])
+
+        setOperations((prevState) => {
+            const newState = {...prevState};
+            newState.setCatOptions = true;
+            return newState
+        });
+    }, [catColumns, fileIsUploaded]);
 
     /* Creates a copy of dataAsJSON saved as dataCopy.
     Creates a copy of dataColumn saved as dataOptions.
@@ -97,39 +154,57 @@ export const DataProvider = ({children}) => {
     to see if a column has indeed only dates inside.
     If an entry cant be parsed into a Date the column is removed from dateOptions */
     useEffect(() => {
-        if (dateColumnsLength > 0) {
-            const data = JSON.parse(JSON.stringify(dataAsJSON));
-            const dateOptions = JSON.parse(JSON.stringify(dateColumns));
+        if (!fileIsUploaded) return
+        if (dateColumns.length === 0) return
 
-            data.forEach(d => {
-                let isDate = true;
-                let invalidColumnIndex = [];
-                for (let i = 0; i < dateColumnsLength; i++){
-                    d[dateColumns[i]] = Date.parse(d[dateColumns[i]]);
-                    
-                    if(isNaN(d[dateColumns[i]])){
-                        isDate = false;
-                        invalidColumnIndex.push(i);
-                    }
+        const data = JSON.parse(JSON.stringify(dataAsJSON));
+        const dateOptions = JSON.parse(JSON.stringify(dateColumns));
+
+        data.forEach(d => {
+            let isDate = true;
+            let invalidColumnIndex = [];
+            for (let i = 0; i < dateColumns.length; i++){
+                d[dateColumns[i]] = Date.parse(d[dateColumns[i]]);
+                
+                if(isNaN(d[dateColumns[i]])){
+                    isDate = false;
+                    invalidColumnIndex.push(i);
                 }
+            }
 
-                for (let i = 0; i < invalidColumnIndex.length; i++){
-                    if(isDate === false){
-                        dateOptions.splice(invalidColumnIndex[i], 1);
-                    }
+            for (let i = 0; i < invalidColumnIndex.length; i++){
+                if(isDate === false){
+                    dateOptions.splice(invalidColumnIndex[i], 1);
                 }
-            });
+            }
+        });
 
-            setDateOptions(dateOptions);
-            setDataCopy(data);
-        }
-    }, [dataAsJSON, dateColumns, dateColumnsLength]);
+        setDateOptions(dateOptions);
+        setDateColumnsLength(dateOptions.length);
+        setDataCopy(data);
+
+        setOperations((prevState) => {
+            const newState = {...prevState};
+            newState.setDateOptions = true;
+            return newState
+        });
+    }, [dataAsJSON, dateColumns, fileIsUploaded]);
+
+    useEffect(() => {
+        if (!fileIsUploaded) return
+
+        const operationsStates = Object.values(operations);
+        const isOperationsDone = operationsStates.every(v => v === true);
+
+        if (isOperationsDone) setIsLoading(!isOperationsDone);
+    }, [operations, fileIsUploaded]);
 
     return (
         <DataContext.Provider 
             value={{
                 fileIsUploaded,
                 setFileIsUploaded,
+                setDataframe,
                 demodata,
                 setDemodata,
                 dataAsJSON,
@@ -143,6 +218,8 @@ export const DataProvider = ({children}) => {
                 catOptions,
                 dateOptions,
                 dataCopy,
+                isLoading,
+                setIsLoading,
             }}
         >
             {children}
