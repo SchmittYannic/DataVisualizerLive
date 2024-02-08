628335ebdfa0a1fb4df081197ded9e864f759d91
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-02-08 19:00:22 +0100

    Makes dimensions of video in videosection responsive

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,20 +1,34 @@
 import { useState, useRef, useEffect } from "react";
 
+import { useWindowSize } from "hooks";
 import {
     vsection_upload_1024x768,
     vsection_data_1024x768,
-    vsection_vis_1024x768
+    vsection_vis_1024x768,
 } from "assets";
 import VideoProgress from "./VideoProgress";
 import "./VideoSection.css";
 
 const VideoSection = () => {
 
+    const windowSize = useWindowSize();
     const uploadwebmRef = useRef(null);
     const datawebmRef = useRef(null);
     const viswebmRef = useRef(null);
 
     const [currentVideo, setCurrentVideo] = useState(null);
+
+    const videoWidth = windowSize.width < 680 ? 344.2
+                    : windowSize.width < 850 ? 640
+                    : windowSize.width < 1050 ? 800 
+                    : windowSize.width < 1140 ? 960 
+                    : 1024;
+
+    const videoHeight = windowSize.width < 680 ? 258.15 
+                    : windowSize.width < 850 ? 480
+                    : windowSize.width < 1050 ? 600
+                    : windowSize.width < 1140 ? 720 
+                    : 768;
 
     const handleVideoEnded = (ref) => {
         const nextVideo = ref.current;
@@ -119,8 +133,8 @@ const VideoSection = () => {
                     <video
                         id="uploadvideo"
                         ref={uploadwebmRef}
-                        width={1024}
-                        height={768}
+                        width={videoWidth}
+                        height={videoHeight}
                         muted="muted"
                         // onCanPlay={() => setCurrentVideo(uploadwebmRef.current)}
                         onEnded={() => handleVideoEnded(datawebmRef)}
@@ -142,8 +156,8 @@ const VideoSection = () => {
                     <video 
                         id="datavideo"
                         ref={datawebmRef}
-                        width={1024}
-                        height={768}
+                        width={videoWidth}
+                        height={videoHeight}
                         muted="muted"
                         onEnded={() => handleVideoEnded(viswebmRef)}
                     >
@@ -164,8 +178,8 @@ const VideoSection = () => {
                     <video 
                         id="visvideo"
                         ref={viswebmRef}
-                        width={1024}
-                        height={768}
+                        width={videoWidth}
+                        height={videoHeight}
                         muted="muted"
                         onEnded={() => handleVideoEnded(uploadwebmRef)}
                     >
