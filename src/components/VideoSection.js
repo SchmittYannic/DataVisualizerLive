a6b6535db4b973e65ed37640482f7766b5039f2b
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-02-20 02:30:16 +0100

    Deletes webms with different resolutions from VideoSection
    Since the VideoSection is only getting displayed on desktop displays only the highest resolution video is sufficient.

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,56 +1,24 @@
 import { useState, useRef, useEffect } from "react";
 
-import { useWindowSize } from "hooks";
 import {
     vsection_upload_1024x768,
     vsection_data_1024x768,
     vsection_vis_1024x768,
-    vsection_upload_960x720,
-    vsection_upload_800x600,
-    vsection_upload_640x480,
-    vsection_data_960x720,
-    vsection_data_800x600,
-    vsection_data_640x480,
-    vsection_vis_960x720,
-    vsection_vis_800x600,
-    vsection_vis_640x480,
 } from "assets";
 import VideoProgress from "./VideoProgress";
 import "./VideoSection.css";
 
 const VideoSection = () => {
 
-    const windowSize = useWindowSize();
     const videoRef = useRef(null);
 
     const [activeTab, setActiveTab] = useState("upload");
 
-    const videoWidth = windowSize.width < 680 ? 344.2
-                    : windowSize.width < 850 ? 640
-                    : windowSize.width < 1050 ? 800 
-                    : windowSize.width < 1140 ? 960 
-                    : 1024;
-
-    const videoHeight = windowSize.width < 680 ? 258.15 
-                    : windowSize.width < 850 ? 480
-                    : windowSize.width < 1050 ? 600
-                    : windowSize.width < 1140 ? 720 
-                    : 768;
-
-    const uploadwebm = windowSize.width < 850 ? vsection_upload_640x480
-                    : windowSize.width < 1050 ? vsection_upload_800x600
-                    : windowSize.width < 1140 ? vsection_upload_960x720 
-                    : vsection_upload_1024x768;
-
-    const datawebm = windowSize.width < 850 ? vsection_data_640x480
-                    : windowSize.width < 1050 ? vsection_data_800x600
-                    : windowSize.width < 1140 ? vsection_data_960x720 
-                    : vsection_data_1024x768;
-
-    const viswebm = windowSize.width < 850 ? vsection_vis_640x480
-                    : windowSize.width < 1050 ? vsection_vis_800x600
-                    : windowSize.width < 1140 ? vsection_vis_960x720 
-                    : vsection_vis_1024x768;
+    const videoWidth = 1024;
+    const videoHeight = 768;
+    const uploadwebm = vsection_upload_1024x768;
+    const datawebm = vsection_data_1024x768;
+    const viswebm = vsection_vis_1024x768;
 
     useEffect(() => {
         if (!videoRef.current) return
