8b6e89db093798292ee201a775558e484e26c518
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-02-22 19:19:42 +0100

    Adds CodeSplitting to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Homepage, ProgressSteps } from "components";
-import { UploadStep, DataStep, VisualizationStep } from "features/datavisualizer";
 import ScrollToTop from "components/ScrollToTop";
+
+const LazyUploadStep = lazy(() => import("features/datavisualizer/UploadStep"));
+const LazyDataStep = lazy(() => import("features/datavisualizer/DataStep"));
+const LazyVisualizationStep = lazy(() => import("features/datavisualizer/VisualizationStep"));
 
 export const steps = [
     {
         stepcount: 1,
         label: "Upload",
-        component: UploadStep,
+        component: LazyUploadStep,
         path: "/DataVisualizer/UploadStep",
     },
     {
         stepcount: 2,
         label: "Datenansicht",
-        component: DataStep,
+        component: LazyDataStep,
         path: "/DataVisualizer/DataStep",
     },
     {
         stepcount: 3,
         label: "Visualisierung",
-        component: VisualizationStep,
+        component: LazyVisualizationStep,
         path: "/DataVisualizer/VisualizationStep",
     },
 ]
@@ -31,7 +35,12 @@ const App = () => {
                 <Routes>
                     <Route exact path="/*" element={<Homepage />}/>
                     {steps.map((step, idx) => (
-                        <Route key={idx} path={step.path} element={<><ProgressSteps /><step.component /></>}/>
+                        <Route key={idx} path={step.path} element={
+                            <Suspense>
+                                <ProgressSteps />
+                                <step.component />
+                            </Suspense>
+                        }/>
                     ))}
                 </Routes>
             </ScrollToTop>
