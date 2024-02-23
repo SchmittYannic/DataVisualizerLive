1c3287159dbaf04b310c77bfcec6305884915e08
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-02-23 23:50:27 +0100

    Fixes import colorlegend in ScatterPlotLogic

--- src/features/charts/scatterplot/ScatterplotLogic.js
+++ src/features/charts/scatterplot/ScatterplotLogic.js
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
-import { colorlegend } from "features/charts/scatterplot/colorlegend";
+import colorlegend from "features/charts/scatterplot/colorlegend";
 
 export const scatterplot = (selection, props) => {
     const { settingsRef, data, placeholderString } = props;
